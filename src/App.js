f8e7b68eb3be7a0ab35b02d6e595fcdc1bfbef2c
fiona <h.natapra@gmail.com>
2017-10-06 14:37:01 -0400

    created SimpleForm class in search_bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import SimpleMap from './components/simple_map';
+import SimpleForm from './components/search_bar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="map">
-        <SimpleMap />
-      </div>
+      <section>
+        <div>
+          <SimpleForm />
+        </div>
+        <div className="map">
+            <SimpleMap />
+        </div>
+      </section>
     );
   }
 }
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,3 +1,38 @@
+import React, { Component } from 'react';
+import PlacesAutocomplete, { geocodeByAddress } from 'react-places-autocomplete';
+
+// const PLACES_API_KEY = 'AIzaSyBfGtGWzVPT6HzuNkcZKfsChurjVVOYTSw';
+
+class SimpleForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { address: 'San Francisco CA' }
+    this.onChange = (address) => this.setState({ address });
+  }
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    geocodeByAddress('Los Angeles, CA')
+  .then(results => console.log('latitude: ' + results[0].geometry.location.lat() + ' longitude: ' + results[0].geometry.location.lng() ))
+  .catch(error => console.error(error))
+  }
+
+  render() {
+    const inputProps = {
+      value: this.state.address,
+      onChange: this.onChange,
+    }
+
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <PlacesAutocomplete inputProps={inputProps} />
+        <button type='submit'>Submit</button>
+      </form>
+    );
+  }
+
+}
 
 
-const PLACES_API_KEY = 'AIzaSyBfGtGWzVPT6HzuNkcZKfsChurjVVOYTSw';
+export default SimpleForm;
